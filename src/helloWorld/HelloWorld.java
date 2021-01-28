90e3d3a2b8486a2af42786afc5ffa8a994162ee4
lover-java <78129468+lover-java@users.noreply.github.com>
2021-01-28 14:50:42 +0800

    Update HelloWorld.java
    test33

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package helloWorld;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello WOrld zhy" );
+		System.out.println("Hello WOrld zhy" );
 	}
 
 }
